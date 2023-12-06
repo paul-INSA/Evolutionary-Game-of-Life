d223264ec86484f48b54c208baa07dd1df06b51b
insamartin <martin.duguey@insa-cvl.fr>
2023-12-06 14:33:56 +0100

    correction test-menu.py

--- graphic/test_menu.py
+++ graphic/test_menu.py
@@ -30,11 +30,11 @@ class Menu(pygame.Surface):
         self.game_screen = pygame_menu.Menu('Gaming',pygame.display.get_surface().get_width(), pygame.display.get_surface().get_height(),theme=self.mytheme1)
         self.new_game = pygame_menu.Menu('New Game', pygame.display.get_surface().get_width(), pygame.display.get_surface().get_height(),theme=self.mytheme2)
         
-        self.load_new_game()
-        self.load_main_menu()
-        self.load_game_screen_menu()
+        #self.load_new_game()
+        #self.load_main_menu()
+        #self.load_game_screen_menu()
 
-    def load_main_menu(self):
+    #def load_main_menu(self):
         self.main_menu.add.label('Evolutionnary game of life',font_size=self.myfontsize*2,align=pygame_menu.locals.ALIGN_CENTER)
         self.main_menu.add.vertical_margin(50)
         self.main_menu.add.button('new game', self.new_game,align=pygame_menu.locals.ALIGN_CENTER)
@@ -42,10 +42,10 @@ class Menu(pygame.Surface):
         self.main_menu.add.button('Quit', pygame.QUIT,align=pygame_menu.locals.ALIGN_CENTER)
         self.main_menu.draw(self.surface)
 
-    def load_game_screen_menu(self):
+    #def load_game_screen_menu(self):
         self.game_screen.add.button('Quit', pygame_menu.events.EXIT)
 
-    def load_new_game(self):
+    #def load_new_game(self):
         self.new_game.add.vertical_margin(30)
         self.new_game.add.text_input('largeur de la carte :', default='100',textinput_id='map_width',input_type=pygame_menu.locals.INPUT_INT)
         self.new_game.add.text_input('hauteur de la carte :', default='100',textinput_id='map_height',input_type=pygame_menu.locals.INPUT_INT)
@@ -76,30 +76,29 @@ class Menu(pygame.Surface):
         self.new_game.add.button('Return to main menu', pygame_menu.events.BACK)
         self.new_game.add.vertical_margin(10)
 
-    # def to_print(self, menu_name: str):
-    #     match menu_name:
-    #         case "main_menu":
-    #             events =  pygame.event.get()
-    #             self.main_menu.update(events)
-    #             self.main_menu.draw(self.surface)
-    #             #self.main_menu.mainloop(self.surface)
-    #         case "game_screen":
-    #             self.game_screen.draw(self.subsurface)
-    #             #self.game_screen.mainloop(self.surface)
-    #         case "new_game":
-    #             self.new_game.draw(self.surface)
-    #             #self.new_game.mainloop(self.subsurface)
+    def to_print(self, menu_name: str):
+        match menu_name:
+            case "main_menu":
+                events =  pygame.event.get()
+                self.main_menu.update(events)
+                self.main_menu.draw(self.surface)
+                #self.main_menu.mainloop(self.surface)
+            case "game_screen":
+                self.game_screen.draw(self.subsurface)
+                #self.game_screen.mainloop(self.surface)
+            case "new_game":
+                self.new_game.draw(self.surface)
+                #self.new_game.mainloop(self.subsurface)
                 
 if __name__ == '__main__':
     pygame.init()
     surface = pygame.display.set_mode((720,480))
     menu=Menu(surface)
-    menu.load_main_menu()
     while True:       
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-        
-                 
+        menu.to_print("main_menu")
+        print('test')         
         # pygame.display.flip()
         pygame.display.update()
